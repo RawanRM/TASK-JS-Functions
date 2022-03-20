bd55516817d26b9feaf0f1306182e3d165eac488
Good Lookin <r_a_msmlm@outlook.com>
2022-03-20 18:47:21 +0300

    ADDED CODE to challenge1.js

--- challenge1.js
+++ challenge1.js
@@ -3,7 +3,11 @@
  * Create a function named `printName`
  * - that just prints your name on the screen
  */
-
+function printName(name) {
+  console.log(`My name is ${name}`);
+}
+let my_name = "Rawan";
+printName(my_name);
 /**
  * Task 2:
  * Create a function named `printAge`
@@ -11,6 +15,12 @@
  * - and prints the age on the screen.
  * - Age = current year - birth
  */
+let year = 2021;
+function printAge(birth_year) {
+  console.log(year - birth_year);
+}
+let my_year = 1995;
+printAge(my_year);
 
 /**
  * Task 3:
@@ -22,6 +32,21 @@
  * -- fr: it should print `Bonjour NAME`
  * -- tr: it should print `Merhaba NAME`
  */
+function printHello(the_name, the_lang) {
+  if (the_lang === "en") {
+    console.log(`Hello ${the_name}`);
+  } else if (the_lang === "es") {
+    console.log(`Hola ${the_name}`);
+  } else if (the_lang === "fr") {
+    console.log(`Bonjur ${the_name}`);
+  } else if (the_lang === "tr") {
+    console.log(`Merhaba ${the_name}`);
+  } else {
+    console.log(`${the_lang} is not a listed language.`);
+  }
+}
+let lang = "en";
+printHello(my_name, lang);
 
 /**
  * Task 4:
@@ -29,3 +54,13 @@
  * - that takes 2 parameters as numbers
  * - should print out the bigger number
  */
+function printMax(num1, num2) {
+  if (num1 > num2) {
+    console.log(num1);
+  } else {
+    console.log(num2);
+  }
+}
+let n1 = 30;
+let n2 = 40;
+printMax(n1,n2);
